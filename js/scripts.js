09ddbbebd0226ff44a59a67648ba4f28107ab115
SHIRLEY ANA OVANDO PAZ <37024731818@jpmorgan.sp>
2018-01-31 21:46:26 -0200

    JS antes de arrow functions

--- js/scripts.js
+++ js/scripts.js
@@ -39,21 +39,21 @@ function atualizarSecao(secao) {
 
     for (var posicao = 0; posicao < listaNotas.contaTotal(); posicao++) {
         if (listaNotas.pega(posicao).editando) {
-            conteudoSecao += '<form class="note">'+
-                                '<input class="note__title" type="text" name="titulo" value="' + listaNotas.listaInterna[posicao].titulo + '" placeholder="Título">'+
-                                '<textarea class="note__body" name="texto" rows="5" placeholder="Criar uma nota...">' + listaNotas.listaInterna[posicao].texto +'</textarea>'+
-                                '<button class="note__control" type="button" onclick="adicionarNota(this.form.titulo, this.form.texto, this.form, ' + posicao +')">'+
-                                    'Concluído'+
-                                '</button>'+
-                             '</form>';
+            conteudoSecao += `<form class="note">'+
+                              <input class="note__title" type="text" name="titulo" value="${notaAtual.titulo}" placeholder="Título">
+                              <textarea class="note__body" name="texto" rows="5" placeholder="Criar uma nota...">' ${notaAtual.texto} +'</textarea>
+                              <button class="note__control" type="button" onclick="adicionarNota(this.form.titulo, this.form.texto, this.form, ${posicao})">
+                                  Concluído
+                              </button>
+                           </form>`;
         } else {
-            conteudoSecao += '<form class="note" onclick="editaFormulario(' + posicao + ')">'+
-                                '<button class="note__control" type="button" onclick="removerNota(event,' + posicao + ')">'+
-                                    '<i class="fa fa-times" aria-hidden="true"></i>'+
-                                '</button>'+
-                                '<h1 class="note__title">' + listaNotas.listaInterna[posicao].titulo + '</h1>'+
-                                '<p class="note__body">' + listaNotas.listaInterna[posicao].texto + '</p>'+
-                             '</form>';
+            conteudoSecao += `<form class="note" onclick="editaFormulario(${posicao})">
+                                <button class="note__control" type="button" onclick="removerNota(event,${posicao})">
+                                    <i class="fa fa-times" aria-hidden="true"></i>
+                                </button>
+                                <h1 class="note__title">${notaAtual.titulo}</h1>
+                                <p class="note__body">${notaAtual.texto}</p>
+                             </form>`;
         }
     }
 
